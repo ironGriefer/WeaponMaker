34b25e3e0ed9a30183ba0c23978c098fd705e9c7
ironGriefer <66088066+ironGriefer@users.noreply.github.com>
2021-09-16 21:54:22 -0400

    started building the framework
    ^

--- weaponmaker.py
+++ weaponmaker.py
@@ -5,64 +5,38 @@ weapon = [
 ]
 '''
 import os
-import tkinter as tk
-from tkinter import *
-from tkinter.ttk import *
-#from pystray import MenuItem as item
-#import pystray
-from PIL import Image, ImageTk
 import random
 import time
+import PySimpleGUI as sg
 import shutil
-import ntpath
+from PIL import Image
 
 template = r"template"
 dest_a = r""
+iconpath = r"resources/icon.ico"
+
+
+
 
 def cloneDatapack():
-    shutil.copytree(os.path.join(template,dest_a,"Weapons"))
+    shutil.copytree(os.path.join(template, dest_a, "Weapons"))
+
 
 def interpretWeapon(weapon):
     print("Interpreting Weapon")
 
+createbutton = r"gui/textures/create.png"
+sg.theme("DarkGrey5")
+windowlayout = [
 
+    [sg.Button("",image_filename=createbutton,button_color=("#262626", "#262626"),border_width=0,key="-CREATEBUTTTON-")]
 
+]
 
-class Melee:
-    weapon = [
+window = sg.Window("Minecraft Weapon Creator",windowlayout,size=(700,600),location=(0,0),background_color="#262626",icon="resources/icon.ico")
 
-    ]
-    def __init__(self, attribute, model, trigger, abil):
-        self.weapon.append(attribute)
-        self.weapon.append(model)
-        self.weapon.append(trigger)
-        self.weapon.append(abil)
-
-
-class Weapon:
-    ## self, <ranged, melee, projectile> [<onhit, rightclick incase of melee>, <raycast, set range, homing in case of ranged>, <linear or homing incase of projectile>]
-    def __init__(self, weapontype="none", attribute="none", model="none", trigger="none", abil="none", subtype="none"):
-        if weapontype == "melee":
-            print("Melee Type Selected.")
-            weapon = Melee(attribute,model,trigger,abil)
-            interpretWeapon(weapon)
-
-
-#testattributes = [
-
-#]
-#testweapon = Weapon("melee",testattributes,"111","hit","explode")
-
-
-root = tk.Tk()
-root.title('Minecraft Weapon Maker')
-iconpath = r"resources/icon.ico"
-p1 = PhotoImage(file="resources/icon.png")
-
-root.iconphoto(False, p1)
-
-# Set the size of the window
-root.geometry("700x350")
-
-
-root.mainloop()
+while True:
+    event, values = window.read()
+    if event == "Ok boomer." or event == sg.WIN_CLOSED:
+        break
+window.close()

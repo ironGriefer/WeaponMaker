067e4afe6a10b7a6118592254c0a7c3827e60381
ironGriefer <66088066+ironGriefer@users.noreply.github.com>
2021-09-17 16:54:49 -0400

    added more stuff
    stuff

--- weaponmaker.py
+++ weaponmaker.py
@@ -9,13 +9,34 @@ import random
 import time
 import PySimpleGUI as sg
 import shutil
+import json
 from PIL import Image
 
 template = r"template"
 dest_a = r""
 iconpath = r"resources/icon.ico"
 
+def JAWGText(text="",color="white",italics=False,bold=False,underlined=False,strikethrough=False,mystery=False):
+    def boolToString(bool):
+        if bool == True:
+            bool = "true"
+        else:
+            bool = "false"
+        return bool
 
+    string = '{"name":"' + text + '","bold":"'+boolToString(bold)+'"}'
+    return string
+
+
+
+print(JAWGText("DAyda"))
+
+class JAWGItem(
+    base_item="minecraft:golden_sword",
+    lore=[
+        [JAWGText("This sword is pretty cool",bold=True,italics=False)]
+    ]):
+    print("")
 
 
 def cloneDatapack():
@@ -27,9 +48,23 @@ def interpretWeapon(weapon):
 
 createbutton = r"gui/textures/create.png"
 sg.theme("DarkGrey5")
+
+preview = []
+
+rightside = [
+
+    [sg.Frame("PREVIEW",preview,size=(305, 350),font=("Minercraftory"))],
+    [sg.Button("",image_filename=createbutton,button_color=("#262626", "#262626"),border_width=0,key="-CREATEBUTTTON-")]
+
+]
+leftside = [
+    [sg.Text("Ttext")]
+]
+
 windowlayout = [
 
-    [sg.Button("",image_filename=createbutton,button_color=("#262626", "#262626"),border_width=0,key="-CREATEBUTTTON-")]
+    [sg.Column(leftside,size=(340,590)),sg.Column(rightside,size=(340,590),vertical_alignment="bottom")]
+
 
 ]
 
